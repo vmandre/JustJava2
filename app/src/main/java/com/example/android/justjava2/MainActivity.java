e2c2aebea6ced98077804cb1cab52bad11b38d8f
Vanderlei André <vmandre@gmail.com>
2016-08-03 21:40:42 -0300

    update

--- app/src/main/java/com/example/android/justjava2/MainActivity.java
+++ app/src/main/java/com/example/android/justjava2/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.justjava2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +42,30 @@ public class MainActivity extends AppCompatActivity {
         Log.v(this.getClass().getName(), "This price is " + price);
 
         String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
+//        displayMessage(priceMessage);
+
+        //Abre um mapa com a localização informada
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("geo:47.6, -122.3"));
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
+        composeEmail(new String[]{"vmandre@gmail.com"}, "Teste de email via App", priceMessage);
+
+    }
+
+    private void composeEmail(String[] addresses, String subject, String text) {
+        Intent email = new Intent(Intent.ACTION_SENDTO);
+        email.setData(Uri.parse("mailto:"));
+        email.putExtra(Intent.EXTRA_EMAIL, addresses);
+        email.putExtra(Intent.EXTRA_SUBJECT, subject);
+        email.putExtra(Intent.EXTRA_TEXT, text);
+
+        if (email.resolveActivity(getPackageManager()) != null) {
+            startActivity(email);
+        }
+
     }
 
     private void displayMessage(String priceMessage) {
@@ -48,17 +75,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String createOrderSummary(int price) {
-        CheckBox whippredCrem = (CheckBox) findViewById(R.id.whipped_crem);
+        CheckBox whippredCream = (CheckBox) findViewById(R.id.whipped_cream);
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
         TextView name = (TextView) findViewById(R.id.name);
         String summaryText = "";
 
-        summaryText += "Name: " + name.getText();
-        summaryText += "\nQuantity: " + quantity;
-        summaryText += "\nTotal: " + price;
-        summaryText += "\nAdd Whipped cream ? " + whippredCrem.isChecked();
-        summaryText += "\nAdd  chocolate ? " + chocolate.isChecked();
-        summaryText += "\nThank you!";
+        summaryText += getString(R.string.hint, name.getText()); //  + ": " + name.getText();
+        summaryText += "\n" + getString(R.string.quantity) + ": " + quantity;
+        summaryText += "\n" + getString(R.string.total) + ": " + NumberFormat.getCurrencyInstance().format(price);
+        summaryText += "\n" + getString(R.string.add_whipped_cream) + " " + whippredCream.isChecked();
+        summaryText += "\n" + getString(R.string.add_chocolate) + " " + chocolate.isChecked();
+        summaryText += "\n" + getString(R.string.thank_you);
 
         return summaryText;
     }
